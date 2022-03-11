1047bde2ffacd832b22721254503f880236da77a
Naveen Kumar G <77113035+onlynavv@users.noreply.github.com>
2022-03-11 16:47:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ import {MongoClient} from "mongodb"
 import { servicesRouter } from "./services.js"
 import { userAuthRouter } from "./userauth.js"
 import { orderRouter } from "./order.js"
+import Razorpay from "razorpay"
 
 dotenv.config()
 
 const app = express()
 
 const PORT = process.env.PORT
+
+// const key_id = "rzp_test_5ndz0S2JkbmF1B"
+// const key_secret = "mL1LHSYvoaMZSIm2uwyHM6r8"
+
+var instance = new Razorpay({
+  key_id:process.env.key_id,
+  key_secret:process.env.key_secret
+});
 
 app.use(cors())
 
@@ -29,6 +38,20 @@ export const client = await createConnection()
 
 app.get("/", (request, response)=>{
     response.send("hai from laundry app")
+})
+
+app.get("/laundry/order/createOrder/:grandTotal", (request, response)=>{
+    let {grandTotal} = request.params
+    const amount = grandTotal * 100
+    const currency = "INR"
+    const receipt = "receipt#123"
+    instance.orders.create({amount, currency, receipt}, (error, order)=>{
+        if(error){
+            return response.send({error:err.message})
+        }else{
+            return response.send(order)
+        }
+    })
 })
 
 app.use("/laundry/user", userAuthRouter)
